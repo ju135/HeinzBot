c7df14a04fe0e93f4ba0cf1359d98026645cb586
ju135 <julian.janisch96@gmail.com>
2019-05-17 00:19:43 +0200

    test2

--- heinzBot.py
+++ heinzBot.py
@@ -93,7 +93,7 @@ def get_news(bot, update):
 def unknown(but, update):
     if not (has_rights(update)):
         return
-    update.message.reply_text("Wos wüsd? Red deitsch mit mir.")
+    update.message.reply_text("Ich nix verstehen. 😢")
 
 
 def bop(bot, update):
