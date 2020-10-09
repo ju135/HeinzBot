06568841988e08497902c3ce9972c36a8df1a994
Philipp Burndorfer <phi.bur@gmx.at>
2020-10-09 21:24:49 +0200

    Removed try catch block and added special case check if only one string without space is entered.

--- modules/translate_bot.py
+++ modules/translate_bot.py
@@ -16,39 +16,39 @@ class TranslateBot(AbstractModule):
     @log_errors()
     @send_action(action=ChatAction.TYPING)
     def translate(self, update: Update, context: CallbackContext):
-        try:
-            self.log(text="Trying to translate something..", logging_type=logging.INFO)
+        self.log(text="Trying to translate something..", logging_type=logging.INFO)
 
-            chat_id = update.message.chat_id
-            text = self.get_command_parameter("/trans", update)
+        chat_id = update.message.chat_id
+        text = self.get_command_parameter("/trans", update)
 
-            if text is not None:
-                langs = googletrans.LANGCODES
-                if text == "languages":
-                    message = "*I schreib da moi de Sprochkürzel ausa, de i kau:*\n"
-                    for key, value in langs.items():
-                        message += ''.join(key + ": " + value + ", ")
+        if text is not None:
+            langs = googletrans.LANGCODES
+            if text == "languages":
+                message = "*I schreib da moi de Sprochkürzel ausa, de i kau:*\n"
+                for key, value in langs.items():
+                    message += ''.join(key + ": " + value + ", ")
 
-                    context.bot.send_message(chat_id=chat_id, text=message, parse_mode=telegram.ParseMode.MARKDOWN)
+                context.bot.send_message(chat_id=chat_id, text=message, parse_mode=telegram.ParseMode.MARKDOWN)
 
+            else:
+                splitted = text.split(" ", 2)
+                self.log(text="splitted contains: " + ''.join(str(x + " ") for x in splitted), logging_type=logging.INFO)
+                langs = langs.values()
+                if len(splitted) == 3 and len(splitted[0]) == 2 and splitted[0] in langs and len(splitted[1]) == 2 and splitted[1] in langs:
+                    langfrom = splitted[0]
+                    langto = splitted[1]
                 else:
-                    splitted = text.split(" ", 2)
-                    self.log(text="splitted contains: " + ''.join(str(x + " ") for x in splitted), logging_type=logging.INFO)
-                    langs = langs.values()
-                    if len(splitted) == 3 and len(splitted[0]) == 2 and splitted[0] in langs and len(splitted[1]) == 2 and splitted[1] in langs:
-                        langfrom = splitted[0]
-                        langto = splitted[1]
-                    else:
-                        langfrom = ""
-                        langto = "de"
-                        splitted = text.split(" ", 1)
+                    langfrom = ""
+                    langto = "de"
+                    splitted = text.split(" ", 1)
 
-                    translator = googletrans.Translator()
-                    if (langfrom and langto):
-                        transtext = translator.translate(text=splitted[2], src=langfrom, dest=langto)
-                        message = "Übersetzt von *" + googletrans.LANGUAGES.get(langfrom).capitalize() + "* zu *" + googletrans.LANGUAGES.get(langto).capitalize() + "* hast des:\n" + transtext.text
-                        context.bot.send_message(chat_id=chat_id, text=message, parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
-                    else:
+                translator = googletrans.Translator()
+                if (langfrom and langto):
+                    transtext = translator.translate(text=splitted[2], src=langfrom, dest=langto)
+                    message = "Übersetzt von *" + googletrans.LANGUAGES.get(langfrom).capitalize() + "* zu *" + googletrans.LANGUAGES.get(langto).capitalize() + "* hast des:\n" + transtext.text
+                    context.bot.send_message(chat_id=chat_id, text=message, parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
+                else:
+                    if len(splitted) > 1:
                         transtext = translator.detect(splitted[1])
                         if transtext.confidence > 0.4:
                             langfrom = transtext.lang
@@ -56,9 +56,7 @@ class TranslateBot(AbstractModule):
                             context.bot.send_message(chat_id=chat_id, text="Übersetzt von " + googletrans.LANGUAGES.get(langfrom).capitalize() + " zu " + googletrans.LANGUAGES.get(langto).capitalize() + " hast des:\n" + transtext.text)
                         else:
                             context.bot.send_message(chat_id=chat_id, text="Oida, i bin ma echt ned sicha wos des Gspeibad hasn soi.. Probiers nuamoi und gib ma vielleicht de Ausgaungssproch mit au!")
-
-            else:
-                update.message.reply_text("I moa du host do wos vagessn Kollege!")
-        except Exception as err:
-            self.log(text="Error: {0}".format(err), logging_type=logging.ERROR)
-            update.message.reply_text("Irgendwos is passiert bitte schau da in Log au!")
+                    else:
+                        context.bot.send_message(chat_id=chat_id, text="Hm, i moa du host do wo a Leerzeichen vagessn, de Wuascht kaun i ned verorbeitn.. Probiers nuamoi!")
+        else:
+            update.message.reply_text("I moa du host do wos vagessn Kollege!")
